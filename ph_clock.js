f73a2a5cd623e4e97fc55b65666f8df9a590e434
Micah Fischer <mfischer981@west-mec.org>
2019-02-14 13:42:18 -0700

    Fixed countdown bug. Added comments.
    Fix issue when countdown reaches zero the minute  and second variable displays as NAN. Added comments to code.

--- ph_clock.js
+++ ph_clock.js
@@ -13,25 +13,35 @@
 
 /* ------------------------------------------------- */
 
-var clockID = setInterval(countdown(), 1000);
+// Set the variable clockID to the value of a setInteral that triggers the countdown function every 1000 secs.
+var clockID = setInterval('countdown()', 1000);
 
-var minsLeft = 0;
-var secsLeft = 15;
-var timeLeft = 60;
+// Assign values to variable of minsLeft, secsLeft and timeLeft
+var minsLeft = 30;
+var secsLeft = 0;
+var timeLeft = minsLeft * 60 + secsLeft;
 
+// Countdown function
 function countdown() {
-    minsString = Math.floor(timeLeft / 60);
-    secsString = timeLeft - (60 * minsLeft);
-    addLeadingZero(minsString);
-    addLeadingZero(secsString);
+    // Rounds mins to whole number
+    minsLeft = Math.floor(timeLeft / 60);
+    // Calculates seconds based on minutes left.
+    secsLeft = timeLeft - 60 * minsLeft;
+    // Adds leading zeroes using leading zero fuction below.
+    var minsString = addLeadingZero(minsLeft);
+    var secsString = addLeadingZero(secsLeft);
+    // Write values to textContent in HTML document by using their IDs.
     document.getElementById("minutes").textContent = minsString;
     document.getElementById("seconds").textContent = secsString;
+    // Trigeger check timer function.
     checkTimer();
+    // Decrement the timeLeft variable by 1.
     timeLeft--;
 }
 
+// When the timer runs out display text in the adjacent HTML before the end of the parent.
 function stopClock() {
-    document.getElementById('TimeHead').insertAdjacentHTML('beforeend', "< br / > (Order Expired),");
+    document.getElementById('TimeHead').insertAdjacentHTML('beforeend', "<br> (Order Expired),");
     clearInterval();
 }
 
